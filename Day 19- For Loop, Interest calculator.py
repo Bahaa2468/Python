a85c73cadab335ebae196755f31cc1a1165c3654
Bahaa2468 <bahaa.mahmoud.gr@gmail.com>
2025-02-02 14:34:41 +0200

    Add files via upload

---
+++ Day 19- For Loop, Interest calculator.py
@@ -0,0 +1,30 @@
+print ("loan Calculator")
+user_loan= int(input ("Loan amount:$ "))
+user_Years= int(input("Duration in years:"))
+user_Interest= float(input("Interest: "))
+requested_loan=user_loan
+payout_months = user_Years * 12
+year=0
+
+for i in range (user_Years):
+    interest = float (user_Interest/100)
+    interest_amount = float(interest*user_loan)
+    user_loan+=interest_amount
+    user_loan=round(user_loan,2)
+    user_loan_interest = float(user_loan + interest_amount)
+    year+=1
+    print("Year",year," the amount is: ",user_loan)
+
+monthly_installment= user_loan/payout_months
+monthly_installment=round(monthly_installment,2)
+net_interest= user_loan-requested_loan
+net_interest=round(net_interest,2)
+print ("""
+       The amount requested: $ """,requested_loan,"""
+       Duration of Years: """,user_Years,"""
+       You will pay in interest: $ """,net_interest,"""
+       The total amount payable: $ """,user_loan,"""
+       Your monthly installement: $ """,monthly_installment)
+print()
+print ("Thank ... You ... For ... running ... my ... Code")
+print ("B...A...H....A...A")
