5d4f46a21bf4cd73d4c3c71c4eefe2ede33816ee
Bahaa2468 <bahaa.mahmoud.gr@gmail.com>
2025-02-03 10:52:51 +0200

    Add files via upload

---
+++ Day 20- 1 to 10 Multplier table.py
@@ -0,0 +1,58 @@
+print ("1 to 10 multiplier Table")
+pName= input ("Enter your name: ")
+print ("-   -   -   -   -   -")
+no0= 0
+print ("No. 0")
+for i in range (0,11):
+    print ("0 X ",i," = ", no0*i)
+print ("-   -   -   -   -   -")
+no1= 1
+print ("No. 1")
+for i in range (0,11):
+    print ("1 X ",i," = ", no1*i)
+print ("-   -   -   -   -   -")
+no2= 2
+print ("No. 2")
+for i in range (0,11):
+    print ("2 X ",i," = ", no2*i)
+print ("-   -   -   -   -   -")
+no3= 3
+print ("No. 3")
+for i in range (0,11):
+    print ("3 X ",i," = ", no3*i)
+print ("-   -   -   -   -   -")
+no4= 4
+print ("No. 4")
+for i in range (0,11):
+    print ("4 X ",i," = ", no4*i)
+print ("-   -   -   -   -   -")
+no5= 5
+print ("No. 5")
+for i in range (0,11):
+    print ("5 X ",i," = ", no5*i)
+print ("-   -   -   -   -   -")
+no6= 6
+print ("No. 6")
+for i in range (0,11):
+    print ("6 X ",i," = ", no6*i)
+print ("-   -   -   -   -   -")
+no7= 7
+print ("No. 7")
+for i in range (0,11):
+    print ("7 X ",i," = ", no7*i)
+print ("-   -   -   -   -   -")
+no8= 8
+print ("No. 8")
+for i in range (0,11):
+    print ("8 X ",i," = ", no8*i)
+print ("-   -   -   -   -   -")
+no9= 9
+print ("No. 9")
+for i in range (0,11):
+    print ("9 X ",i," = ", no9*i)
+print ("-   -   -   -   -   -")
+no10= 10
+print ("No. 10")
+for i in range (0,11):
+    print ("10 X ",i," = ", no10*i)
+print ("-   -   -   -   -   -")
