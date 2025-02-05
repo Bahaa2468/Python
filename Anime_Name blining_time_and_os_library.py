5a0fd514a9e9a4146b0a9afb7d6594586efa86bc
Bahaa2468 <bahaa.mahmoud.gr@gmail.com>
2025-02-05 13:57:52 +0200

    Add files via upload

---
+++ Anime_Name blining_time_and_os_library.py
@@ -0,0 +1,25 @@
+
+def blinking():
+    import os,time
+    print ("")
+    time.sleep(1)
+    os.system("cls")
+    print ("B...")
+    time.sleep(1)
+    os.system("cls")
+    print ("    A...")
+    time.sleep(1)
+    os.system("cls")
+    print ("        H...")
+    time.sleep(1)
+    os.system("cls")
+    print ("            A...")
+    time.sleep(1)
+    os.system("cls")
+    print ("                A...")
+    time.sleep(1)
+    os.system("cls")
+    name="Bahaa"
+    return name
+
+print (blinking())
